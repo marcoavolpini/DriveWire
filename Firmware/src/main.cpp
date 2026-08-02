7b361e5fbb816fc9887a36dc66b92bae92dd8f3e
Marco Volpini <145407245+marcoavolpini@users.noreply.github.com>
2026-08-01 23:38:18 -0400

    Explicit Wire I2C implementation

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -5,41 +5,50 @@
 #include <serial_interface.h>
 #include <drivewire_state.h>
 
+#include <Wire.h>
+
 DriveWireState state;
+
+constexpr int SDA_PIN = 7;
+constexpr int SCL_PIN = 15;
 
 void setup(){
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("DriveWire Motor and Sensor Firmware Starting...");
+  Serial.println("DriveWire I2C Firmware Starting...");
 
-  initializeMotors();
-  initializeSensors(state);
-
+  Wire.begin(SDA_PIN, SCL_PIN);
 }
 
 void loop() {
 
   delay(1000);
 
-  // first sampled reading
+  // getting the ESP32 ready to start by sending the INA219 address and a Write bit
+  Wire.beginTransmission(0x40);
 
-  if(refreshDistanceTelemetry(state)) {
-    
-    Serial.print("Distance in front of chassis in mm: ");
-    Serial.println(state.distanceMm);
+  Wire.write(0x2); // queues the desired register to send to the INA219 (for bus voltage)
 
-  } else if(!state.tofOnline) { 
-    Serial.println("VL53L0X Offline");
-    Serial.println("Failed to update electrical telemetry");
+  Wire.endTransmission(false); // false tells ESP32 not to send STOP
+  // this sends everything and gets ACK
 
-    Serial.println("Last valid reading: ");
-    Serial.println(state.distanceMm);
-  } else {
-    Serial.println("Invalid measurement taken; object could be too far");
+  if (Wire.requestFrom(0x40, 2) != 2) { return; }
+  // sends 0x40 again, and a read bit. Wants 2 bytes of data from the register.
 
-    Serial.println("Last valid reading: ");
-    Serial.println(state.distanceMm);
-  }
+  uint8_t MSB = Wire.read();
+  uint8_t LSB = Wire.read();
+
+  uint16_t MSB_shifted = (MSB << 8);  // bitshift MSB by 8 to make room for the LSB
+  uint16_t rawBusVoltage = (MSB_shifted | LSB);
+
+  uint16_t voltageCounts = (rawBusVoltage >> 3); // shave off the status bits
+
+  float busVoltage = (voltageCounts * 0.004); // multiply the voltage count by 4 mV for total
+
+
+  Serial.println(busVoltage);
+
+  delay(2000);
 
 }
